a8e0f2c23d89855f18c9a6eeaf30b3cc7c881366
jawher <jawher.khalifa@esprit.tn>
2019-07-18 11:58:06 +0100

    gg

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -25,16 +25,6 @@ class Categorie
      */
     private $nom;
 
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Service", mappedBy="services")
-     */
-    private $services;
-
-    public function __construct()
-    {
-        $this->services = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
@@ -52,34 +42,4 @@ class Categorie
         return $this;
     }
 
-    /**
-     * @return Collection|service[]
-     */
-    public function getServices(): Collection
-    {
-        return $this->services;
-    }
-
-    public function addService(service $service): self
-    {
-        if (!$this->services->contains($service)) {
-            $this->services[] = $service;
-            $service->setServices($this);
-        }
-
-        return $this;
-    }
-
-    public function removeService(service $service): self
-    {
-        if ($this->services->contains($service)) {
-            $this->services->removeElement($service);
-            // set the owning side to null (unless already changed)
-            if ($service->getServices() === $this) {
-                $service->setServices(null);
-            }
-        }
-
-        return $this;
-    }
 }
