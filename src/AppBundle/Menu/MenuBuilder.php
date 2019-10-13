7c6fc5a8a27d271c184db13dbc1a7a77bb7aaa25
Ahmed BG <boughattas50@gmail.com>
2019-10-13 16:01:12 +0100

    Dashboard

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -5,7 +5,8 @@ use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-class MenuBuilder
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class MenuBuilder 
 {
     private $factory;
 
@@ -19,26 +20,51 @@ class MenuBuilder
         $this->factory = $factory;
     }
 
-    public function createMainMenu(array $options)
+    public function createMainMenu()
     {
         $menu = $this->factory->createItem('root');
-
-        $menu->addChild('Home', ['route' => 'service']);
-     
-        // ... add more children
-
         return $menu;
     }
     public function createSidebarMenu(array $options)
     {
         $menu = $this->factory->createItem('root');
-
-        $menu->addChild('Products', ['route' => 'service']);
-        $menu->addChild('Attributes', ['route' => 'service']);
+       // $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->addChild('Products', ['route' => 'products']);
+        $menu->addChild('Attributes', ['route' => 'attributes']);
         $menu->addChild('Options', ['route' => 'service']);
         $menu->addChild('Association types', ['route' => 'service']);
+       /* foreach ($menu as $child) {
+            $child->setLinkAttribute('class', 'nav-link')
+                ->setAttribute('class', 'nav-item');
+        }*/
         // ... add more children
 
+        return $menu;
+    }
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+    	$menu = $factory->createItem('root');
+    	$menu->setChildrenAttribute('class', 'nav navbar-nav');
+		$menu->addChild('Projects', array('route' => 'acme_hello_projects'))
+			->setAttribute('icon', 'fa fa-list');
+		$menu->addChild('Employees', array('route' => 'acme_hello_employees'))
+			->setAttribute('icon', 'fa fa-group');
+        return $menu;
+    }
+    public function userMenu(array $options)
+    {
+    	$menu = $this->factory->createItem('root');
+    	$menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
+    	/*
+    	You probably want to show user specific information such as the username here. That's possible! Use any of the below methods to do this.
+    	if($this->container->get('security.context')->isGranted(array('ROLE_ADMIN', 'ROLE_USER'))) {} // Check if the visitor has any authenticated roles
+    	$username = $this->container->get('security.context')->getToken()->getUser()->getUsername(); // Get username of the current logged in user
+    	*/	
+		$menu->addChild('User', array('label' => 'Hi visitor'))
+			->setAttribute('dropdown', true)
+			->setAttribute('icon', 'fa fa-user');
+		$menu['User']->addChild('Edit profile', array('route' => 'acme_hello_profile'))
+			->setAttribute('icon', 'fa fa-edit');
         return $menu;
     }
 }
--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Response;
 class ServiceController extends AbstractController
 {
     /**
@@ -15,5 +15,33 @@ class ServiceController extends AbstractController
         return $this->render('Admin/menu.html.twig', [
             'controller_name' => 'ServiceController',
         ]);
+    }
+    /**
+     * @Route("/Products",name="products")
+     */
+    public function products(){
+        return $this->render('Admin/products.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+
+    }
+    /**
+     * @Route("/Attributes",name="attributes")
+     */
+    public function attributes(){
+        return $this->render('Admin/attributes.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+
+    }
+    /**
+     * @Route("/grid",name="grid")
+     * 
+     */
+    public function grid(){
+        return $this->render('Admin/gridProduct.html.twig', [
+            'controller_name' => 'ServiceController',
+        ]);
+
     }
 }
